25f3b962a1160faf8943c9a7bffe26851346034b
charliewambo31 <e2295844@cmaisonneuve.qc.ca>
2023-05-30 22:46:48 -0400

    commit

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ function field_focus(field, email)
   }
 
 //Fade in dashboard box
-$(document).ready(function(){
+(document).ready(function(){
     $('.box').hide().fadeIn(1000);
     });
 
 //Stop click event
-$('a').click(function(event){
+('a').click(function(event){
     event.preventDefault(); 
 	});
 
